3a962f004310bf339156cbe9e0d9013bb2b04462
madhur19171 <madhur891@gmail.com>
2024-11-30 15:02:12 +0100

    Using folio_isolate_lru to remove folio from LRU list and added kfunc for pinning

--- mm/bpf_mm_kfuncs.c
+++ mm/bpf_mm_kfuncs.c
@@ -3,6 +3,7 @@
 #include <linux/btf_ids.h>
 #include <linux/mm.h>
 #include <linux/mm_inline.h>
+#include "internal.h"
 
 __bpf_kfunc_start_defs();
 
@@ -81,17 +82,117 @@ __bpf_kfunc int bpf_insert_file_vaddr_into_inactive_list(int pid, unsigned long 
 		return -EINVAL;
 	}
 
-	// Removing from current list
-	lruvec_del_folio(lruvec, folio);
-
-	// Adding to INACTIVE_FILE list
 	enum lru_list lru = folio_lru_list(folio);
-	update_lru_size(lruvec, LRU_INACTIVE_FILE, folio_zonenum(folio),
-			folio_nr_pages(folio));
-	if (lru != LRU_UNEVICTABLE)
+
+	if (lru != LRU_UNEVICTABLE) {
+		// Removing from current list
+		if (folio_isolate_lru(folio)) {
+			printk("Folio for VA(0x%lx) and PID(%d) Removed From Current LRU List!\n", vaddr, pid);
+		}
+
+		// Adding to INACTIVE_FILE list
+		update_lru_size(lruvec, LRU_INACTIVE_FILE, folio_zonenum(folio),
+				folio_nr_pages(folio));
+
 		list_add(&folio->lru, &lruvec->lists[LRU_INACTIVE_FILE]);
 
-	printk("Folio for VA(0x%lx) and PID(%d) Added to Inactive List!\n", vaddr, pid);
+		printk("Folio for VA(0x%lx) and PID(%d) Added to Inactive List!\n", vaddr, pid);
+	}
+
+	return 0;
+}
+
+__bpf_kfunc int bpf_pin_file_vaddr(int pid, unsigned long vaddr) {
+	struct mm_struct *mm;
+	struct page *page;
+	struct pid *spid;
+	struct task_struct *task;
+	pte_t *pte;
+	spinlock_t *ptl;
+	struct folio *folio;
+	struct mem_cgroup *memcg;
+	pg_data_t *pgdat;
+	struct lruvec *lruvec;
+
+	// Validate inputs
+	if (!vaddr) {
+		printk("VA is NULL\n");
+		return -EINVAL;
+	}
+
+	spid = find_get_pid(pid);
+	if (!spid) {
+		printk("pid struct is NULL\n");
+		return -EINVAL;
+	}
+
+	task = get_pid_task(spid, PIDTYPE_PID);
+	if (!task) {
+		printk("task struct is NULL\n");
+		return -EINVAL;
+	}
+
+	mm = get_task_mm(task);
+	if (!mm) {
+		printk("mm struct is NULL\n");
+		return -ESRCH; // No memory structure for this task
+	}
+
+	memcg = get_mem_cgroup_from_mm(mm);
+	if (!memcg) {
+		printk("memcg struct is NULL\n");
+		return -EINVAL;
+	}
+
+	pte = get_locked_pte(mm, vaddr, &ptl);
+	if (!pte) {
+		printk("pte is NULL\n");
+		return -EINVAL;
+	}
+
+	page = pte_page(*pte);
+	if (!page) {
+		printk("page is NULL\n");
+		return -EINVAL;
+	}
+
+	// Release PTE lock
+	pte_unmap_unlock(pte, ptl);
+
+	folio = page_folio(page);
+	if (!folio) {
+		printk("folio is NULL\n");
+		return -EINVAL;
+	}
+
+	pgdat = page_pgdat(&folio->page);
+	if (!pgdat) {
+		printk("pgdat is NULL\n");
+		return -EINVAL;
+	}
+
+	lruvec = mem_cgroup_lruvec(memcg, pgdat);
+	if (!lruvec) {
+		printk("lruvec is NULL\n");
+		return -EINVAL;
+	}
+
+	enum lru_list lru = folio_lru_list(folio);
+
+	if (lru != LRU_UNEVICTABLE) {
+		// Removing from current list
+		if (folio_isolate_lru(folio)) {
+			printk("Folio for VA(0x%lx) and PID(%d) Removed From Current LRU List!\n", vaddr, pid);
+		}
+
+		// Adding to UNEVICTABLE list
+		update_lru_size(lruvec, LRU_UNEVICTABLE, folio_zonenum(folio),
+				folio_nr_pages(folio));
+
+		list_add(&folio->lru, &lruvec->lists[LRU_UNEVICTABLE]);
+
+		printk("Folio for VA(0x%lx) and PID(%d) Added to Unevictable List!\n", vaddr, pid);
+	}
 
 	return 0;
 }
@@ -100,15 +201,8 @@ __bpf_kfunc_end_defs();
 
 BTF_KFUNCS_START(bpf_mm_kfunc_set_ids)
 BTF_ID_FLAGS(func, bpf_insert_file_vaddr_into_inactive_list)
+BTF_ID_FLAGS(func, bpf_pin_file_vaddr)
 BTF_KFUNCS_END(bpf_mm_kfunc_set_ids)
-
-// static int bpf_mm_kfuncs_filter(const struct bpf_prog *prog, u32 kfunc_id)
-// {
-// 	if (!btf_id_set8_contains(&bpf_mm_kfunc_set_ids, kfunc_id) ||
-// 	    prog->type == BPF_PROG_TYPE_LSM)
-// 		return 0;
-// 	return -EACCES;
-// }
 
 static const struct btf_kfunc_id_set bpf_mm_kfunc_set = {
 	.owner = THIS_MODULE,
